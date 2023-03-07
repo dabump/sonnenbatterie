86c86bf8dc30d661fe6c13bfe0c08a276c1477fe
Martin Coetzee <martincoetzee@Martins-MacBook-Pro.local>
2023-03-08 08:50:29 +1000

    addressed test case to include 24 hours

--- internal/notification/rules_test.go
+++ internal/notification/rules_test.go
@@ -2,14 +2,19 @@ package notification
 
 import (
 	"testing"
+	"time"
 
 	"github.com/dabump/sonnenbatterie/internal/trend"
 )
 
 func Test_ruleEngine_dispatchNotification(t *testing.T) {
+	twentyFourHourDuration, _ := time.ParseDuration("-24h")
+	before24Hours := time.Now().Add(twentyFourHourDuration)
+
 	type fields struct {
 		notifiedOnFull        bool
 		notifiedOnEmpty       bool
+		lastReset             time.Time
 		lastNotificationTrend trend.Trend
 	}
 	type args struct {
@@ -24,73 +29,103 @@ func Test_ruleEngine_dispatchNotification(t *testing.T) {
 		{
 			name: "100% not yet notified",
 			fields: fields{
-				notifiedOnFull: false,
-				notifiedOnEmpty: false,
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       false,
+				lastReset:             time.Now(),
 				lastNotificationTrend: trend.Upward,
 			},
 			want: true,
 			args: args{
-				values: []int{100,99},
+				values: []int{100, 99},
 			},
 		},
 		{
 			name: "100% already notified",
 			fields: fields{
-				notifiedOnFull: true,
-				notifiedOnEmpty: false,
+				notifiedOnFull:        true,
+				notifiedOnEmpty:       false,
+				lastReset:             time.Now(),
 				lastNotificationTrend: trend.Upward,
 			},
 			want: false,
 			args: args{
-				values: []int{100,99},
+				values: []int{100, 99},
+			},
+		},
+		{
+			name: "100% already notified - 24 hours since last notification",
+			fields: fields{
+				notifiedOnFull:        true,
+				notifiedOnEmpty:       false,
+				lastReset:             before24Hours,
+				lastNotificationTrend: trend.Upward,
+			},
+			want: true,
+			args: args{
+				values: []int{100, 99},
 			},
 		},
 		{
 			name: "0% not yet notified",
 			fields: fields{
-				notifiedOnFull: false,
-				notifiedOnEmpty: false,
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       false,
+				lastReset:             time.Now(),
 				lastNotificationTrend: trend.Downward,
 			},
 			want: true,
 			args: args{
-				values: []int{0,1,2},
+				values: []int{0, 1, 2},
 			},
 		},
 		{
 			name: "0% already notified",
 			fields: fields{
-				notifiedOnFull: false,
-				notifiedOnEmpty: true,
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       true,
+				lastReset:             time.Now(),
 				lastNotificationTrend: trend.Downward,
 			},
 			want: false,
 			args: args{
-				values: []int{0,1,2},
+				values: []int{0, 1, 2},
+			},
+		},
+		{
+			name: "0% already notified - 24 hours since last notification",
+			fields: fields{
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       true,
+				lastReset:             before24Hours,
+				lastNotificationTrend: trend.Downward,
+			},
+			want: true,
+			args: args{
+				values: []int{0, 1, 2},
 			},
 		},
 		{
 			name: "Upper threshold reacher",
 			fields: fields{
-				notifiedOnFull: false,
-				notifiedOnEmpty: false,
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       false,
 				lastNotificationTrend: trend.Downward,
 			},
 			want: true,
 			args: args{
-				values: []int{upperThresholdNotification, upperThresholdNotification-1, upperThresholdNotification-2},
+				values: []int{upperThresholdNotification, upperThresholdNotification - 1, upperThresholdNotification - 2},
 			},
 		},
 		{
 			name: "Lower threshold reacher",
 			fields: fields{
-				notifiedOnFull: false,
-				notifiedOnEmpty: false,
+				notifiedOnFull:        false,
+				notifiedOnEmpty:       false,
 				lastNotificationTrend: trend.Upward,
 			},
 			want: true,
 			args: args{
-				values: []int{lowerThresholdNotification, lowerThresholdNotification+1, lowerThresholdNotification+2},
+				values: []int{lowerThresholdNotification, lowerThresholdNotification + 1, lowerThresholdNotification + 2},
 			},
 		},
 	}
@@ -100,6 +135,7 @@ func Test_ruleEngine_dispatchNotification(t *testing.T) {
 				notifiedOnFull:        tt.fields.notifiedOnFull,
 				notifiedOnEmpty:       tt.fields.notifiedOnEmpty,
 				lastNotificationTrend: tt.fields.lastNotificationTrend,
+				lastReset:             tt.fields.lastReset,
 			}
 			if got := r.dispatchNotification(tt.args.values); got != tt.want {
 				t.Errorf("ruleEngine.dispatchNotification() = %v, want %v", got, tt.want)
